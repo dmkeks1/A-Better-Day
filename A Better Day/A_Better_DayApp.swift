0c686feac7f138e6a500dad76212cf7fa6e54b67
Dominik Zehe <zehedominik@googlemail.com>
2024-11-06 18:26:05 +0100

    Initial Commit

---
+++ A Better Day/A_Better_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Better_DayApp.swift
+//  A Better Day
+//
+//  Created by Dominik Zehe on 06.11.24.
+//
+
+import SwiftUI
+
+@main
+struct A_Better_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
